8c868798804742ac8c04a770ad6af65d2c1c464a
cocagolau <cocagolau@gmail.com>
2014-08-30 11:49:46 +0900

    issue #1 마우스 클릭시 sidebar가 보이거나 안보이도록 구현

---
+++ final/static/javascripts/app/app.js
@@ -0,0 +1,67 @@
+(function() {
+	'use strict';
+
+	var document = window.document;
+	var console = window.console;
+	var APP = window.APP || {};
+
+	APP.sidebar = APP.sidebar || {};
+ 	
+
+ 	/*
+	@param: config <object>
+	config.viewSelector: <string: selector>
+	config.buttonSelector: <string: selector>
+ 	*/
+
+ 	function Sidebar(config) {
+ 		this.viewEl = null;
+ 		this.buttonEl = null;
+
+ 		this.init(config);
+ 	}
+ 	var fn = Sidebar.prototype;
+
+ 	fn.init = function(config) {
+ 		this.viewEl = document.querySelector(config.viewSelector);
+ 		this.viewEl.addEventListener('transitionend', this.listen.bind(this), false);
+
+ 		this.buttonEl = document.querySelector(config.buttonSelector);
+
+ 		// event listening 시작
+ 		this.listen();
+ 	};
+
+ 	fn.listen = function() {
+ 		this.bindedToggleFn = this.toggle.bind(this);
+ 		this.buttonEl.addEventListener('click', this.bindedToggleFn, false);
+ 	};
+
+ 	fn.unlisten = function() {
+ 		this.buttonEl.removeEventListener('click', this.bindedToggleFn, false);
+ 		this.bindedToggleFn = undefined;
+ 	};
+ 
+ 	fn.toggle = function() {
+ 		// event listening 중단
+ 		this.unlisten();
+
+ 		var classList = this.viewEl.classList;
+
+ 		// 이전 tansition관련 class존재시 제거, 아니면 추가
+ 		var method = (classList.contains('open'))?'remove':'add';
+ 		classList[method]('open');
+ 	};
+
+
+	APP.sidebar = Sidebar;
+
+	// 글로벌 객체에 모듈을 프로퍼티로 등록한다.
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = APP;
+		// browser export
+	} else {
+		window.APP = APP;
+	}
+
+}(this));
